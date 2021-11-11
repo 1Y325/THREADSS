99db3b91ceac4db672e93f3b1b847ec29d844e00
Yash Gupta <yashg3600@gmail.com>
2021-11-11 10:14:25 +0530

    list integer

---
+++ src/exceptionhandling.java
@@ -0,0 +1,17 @@
+//import jdk.internal.icu.text.UnicodeSet;
+
+import java.util.*;
+
+public class exceptionhandling {
+    public static void main(String[] args) {
+
+        List<Integer> l = new ArrayList<>();
+          l.add(9);
+          //l.set(2,1000);
+        System.out.println(l);
+
+
+
+
+    }
+}
